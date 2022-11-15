05fe658160cb2251f680f4b2e6ebc54e3d0d674d
parksangwon <whlsls3377@naver.com>
2022-11-15 16:23:09 +0900

    day13 로또번호 셍성기

---
+++ day13/Lotto.java
@@ -0,0 +1,20 @@
+package day13;
+import java.util.*; //java.util 패키지에 있는 모든 클래스 import
+/*
+    1. 6개의 로또 번호를 저장할 배열
+    2. 보너스 숫자를 저장할 변수
+    3. 사용자에게 6개의 숫자를 입력 받아 저장할 배열
+    4. 사용자에게 보너스를 입력받아 저장할 변수
+    5. 동일한 숫자가 몇 개인지 카운팅할 변수
+ */
+public class Lotto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random=new Random();
+        int lottoAr[]=new int[6];
+        int userAr[]=new int[6];
+        int bonus,bonusUser;
+        int count=0;
+
+    }
+}
